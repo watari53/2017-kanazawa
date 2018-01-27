44ca53c634cb1f8dcc26c092e438d10603757f85
watari53 <watari53@gmail.com>
2018-01-28 00:27:02 +0900

    経路表示機能実装、データの整合性合わせ完了、時刻表示修正

--- www/components/main.js
+++ www/components/main.js
@@ -10,6 +10,11 @@
 // traffic_jam   = "./images/traffic_jam.svg";
 // no_bicycle    = "./images/no_bicycle.svg";
 // may_no_bicycle = "./images/may_no_bicycle.svg";
+var show_route = ["徒歩", "自転車"];
+var DEFAULT_SRC_MSG = "現在地";
+var DEFAULT_DEST_MSG = "到着地を選択";
+var DEFAULT_TIME = "現在時刻";
+var APP_NAME = "金沢乗換案内";
 wp_color    = {
                 walk: "#ccc",
               };
@@ -34,12 +39,16 @@ ons.bootstrap()
 
     service.setSample = function(data){
       sample = data;
+      sample.surroundings.unshift({spot_name: DEFAULT_SRC_MSG, distance: ""});
     };
 
     service.getSurroundings = function() {
       return sample.surroundings;
     };
     service.setHistory = function(spot_name) {
+      if(spot_name === DEFAULT_SRC_MSG) {
+        return;
+      }
       var history = localStorage.getItem('history');
       if(history == null) {
         localStorage.setItem('history', spot_name);
@@ -72,14 +81,10 @@ ons.bootstrap()
     return service;
   })
   .controller('AppController', function($scope, $http, DataService) {
-    $scope.application_name = "金沢乗換案内";
-    var DEFAULT_SRC_MSG = "現在地";
-    var DEFAULT_DEST_MSG = "到着地を選択";
-    var DEFAULT_TIME = "現在時刻";
+    $scope.application_name = APP_NAME;
     $scope.search = {src: DEFAULT_SRC_MSG, dest: DEFAULT_DEST_MSG};
     $scope.time = DEFAULT_TIME;
     $scope.people_n = 1;
-    var search_params = {src: "", dest: "", time: "", people_n: ""};
 
     $http.get('sample.json').then(function(response){
       DataService.setSample(response.data);
@@ -161,24 +166,51 @@ ons.bootstrap()
   })
   .controller('TimeLineDetailController', function(DataService) {
     this.detail = navi.topPage.data.timeline_detail;
-    this.go_map = function(waypoint){
-      navi.pushPage('map.html', {data: {waypoint: waypoint}});
+    var waypoint = this.detail.waypoint;
+    
+    function getSrcLocation() {
+      return {name: "現在地", lat: 36.578268, lng: 136.648035};
+    }
+    // show route if transportation_text exist in show_route
+    this.showRoute = function(transportation_text) {
+        if(show_route.indexOf(transportation_text) !== -1) {
+          return true;
+        }
+    };
+    // wapoint_index: 0,1,...n
+    this.go_map = function(w_index){
+      var srcLocation;
+      if(w_index === 0) {
+        srcLocation = getSrcLocation();
+      } else {
+        var i = w_index - 1;
+        srcLocation = {name: waypoint[i].spot_name, lat: waypoint[i].lat, lng: waypoint[i].lng};
+      }
+      var destLocation = {name: waypoint[w_index].spot_name, lat: waypoint[w_index].lat, lng: waypoint[w_index].lng};
+      navi.pushPage('map.html', {data: {src: srcLocation, dest: destLocation}});
     };
     this.go_spot = function(spot_name) {
       navi.pushPage('spot.html', {data: {spot_name: spot_name}});
     };
   })
   .controller('MapController', function() {
-    var myLatLng = {lat: 36.578268, lng: 136.648035};
-    wp = navi.topPage.data.waypoint;
-    var destLatLng = {lat: wp.lat, lng: wp.lng};
-    this.map_url = wp.map_url;
+    var srcLatLng = navi.topPage.data.src;
+    var destLatLng = navi.topPage.data.dest;
+    this.map_url = "http://maps.google.com/maps?saddr=" + srcLatLng.lat + "," + srcLatLng.lng + "&daddr=" + destLatLng.lat + "," + destLatLng.lng;
+    console.log(srcLatLng.name +","+ srcLatLng.lat + ","+ srcLatLng.lng);
+    console.log(destLatLng.name +","+ destLatLng.lat+ ","+ destLatLng.lng);
+    
+    this.goExMap = function(url) {
+      window.open(url, '_system', 'location=yes');
+      return false;
+    };
+
     //Google mapの設定
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 7,
-      center: myLatLng,
+      center: srcLatLng,
     });
     directionsDisplay.setMap(map);
     calculateAndDisplayRoute(directionsService, directionsDisplay);
@@ -187,7 +219,7 @@ ons.bootstrap()
       // var selectedMode = document.getElementById('mode').value;
       var selectedMode = "WALKING";
       directionsService.route({
-        origin: myLatLng,
+        origin: srcLatLng,
         destination: destLatLng,
         // Note that Javascript allows us to access the constant
         // using square brackets and a string value as its
